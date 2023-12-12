977c336363bd8d3667ad52c846143f28e99aa912
gabriel <gabrielfelipe941@yahoo.com.br>
2023-12-12 15:01:49 -0300

    Configure storybook and templates_generator

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,11 @@
-export const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+import * as S from "./styles"
+
+export const Main = ({
+  title = "React Avançado",
+  description = "Typescript, ReactJS, NextJS e Styled Components"
+}) => (
+  <S.Wrapper>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+  </S.Wrapper>
 )
